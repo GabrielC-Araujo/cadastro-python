fe17eb0713fc0878dc1ca51f3710f9a96cc21a61
Gabriel Cruz <167445202+GabrielC-Araujo@users.noreply.github.com>
2024-05-17 16:40:06 -0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from cliente.cliente import Cliente
-
-cliente1 = Cliente('Nix', '11144477735', 'nix@teste.com', 11991234567, 'Av. Não Sei 24')
-cliente2 = Cliente('Marcos', '11144477735', 'marcos@teste.com', 11991234567, 'Av. Não Sei 57')
-
-
-print(cliente1)
-print(cliente2)
